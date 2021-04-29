aedf684fbaa33cc89710b11a88ab9aa56d9ec8e1
Pavel Gun <pavel_gun@epam.com>
2021-04-29 10:15:16 +0300

    fix / build.gradle

--- src/main/java/org/example/untitled/components/Component.java
+++ src/main/java/org/example/untitled/components/Component.java
@@ -1,8 +1,8 @@
 package org.example.untitled.components;
 
 import org.example.untitled.Handler;
-import org.example.untitled.modules.Module;
 import org.example.untitled.db.DocumentDao;
+import org.example.untitled.modules.Module;
 import org.example.untitled.mq.Mq;
 
 import javax.inject.Singleton;
