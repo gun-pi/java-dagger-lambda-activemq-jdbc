5bdc32afa8dbf4dddf6c8ca7192252c3032d2fc6
Pavel Gun <pavel_gun@epam.com>
2021-04-30 13:11:14 +0300

    fix / tests

--- src/test/java/HandlerTest.java
+++ src/test/java/HandlerTest.java
@@ -1,46 +1,15 @@
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
-/*import org.example.untitled.db.DocumentDao;
-import org.example.untitled.db.DocumentEntity;*/
+import com.amazonaws.services.lambda.runtime.Context;
 import org.example.untitled.Handler;
-import org.example.untitled.db.DocumentDao;
-import org.example.untitled.db.DocumentEntity;
-import org.example.untitled.mq.Mq;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import javax.inject.Inject;
-import javax.jms.JMSException;
-import java.sql.SQLException;
 
 public class HandlerTest {
 
-
-
     @Test
     void test() {
-
         Handler handler = new Handler();
-        handler.handleRequest("test", null);
-
-
-        /*String input = "test";
-        String message = input;
-        *//*try {
-            mq.sendMessage(input);
-            message = mq.receiveMessage();
-        } catch (JMSException e) {
-            e.printStackTrace();
-        }*//*
-
-        System.out.println(message);
-
-        Long id = null;
-        try {
-            id = documentDao.save(new DocumentEntity(message));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(id + ": here I am");*/
+        Context context = new TestContext();
+        final String id = handler.handleRequest("test", context);
+        Assertions.assertTrue(id.matches("[-+]?\\d+"));
     }
 }
