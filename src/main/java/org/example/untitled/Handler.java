94786da0af7f2e37262f48f81ba962782a86717c
Pavel Gun <pavel_gun@epam.com>
2021-05-11 11:14:03 +0300

    fix / refactoring

--- src/main/java/org/example/untitled/Handler.java
+++ src/main/java/org/example/untitled/Handler.java
@@ -1,19 +1,23 @@
 package org.example.untitled;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.example.untitled.components.DaggerComponent;
 import org.example.untitled.db.DocumentDao;
 import org.example.untitled.db.DocumentEntity;
 import org.example.untitled.mq.Mq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
 public class Handler implements RequestHandler<String, String> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Handler.class);
+
     @Inject
     DocumentDao documentDao;
+
     @Inject
     Mq mq;
 
@@ -23,15 +27,14 @@ public class Handler implements RequestHandler<String, String> {
 
     @Override
     public String handleRequest(final String input, final Context context) {
-        final LambdaLogger LOG = context.getLogger();
-        LOG.log("Request handler is starting with input string: " + input);
+        LOG.info("Request handler is starting with input string: " + input);
 
         String message;
         try {
             mq.sendMessage(input);
             message = mq.receiveMessage();
         } catch (Exception e) {
-            LOG.log("Exception in handleRequest method occurred during JMS interaction: " + e);
+            LOG.error("Exception in handleRequest method occurred during JMS interaction: " + e);
             throw new RuntimeException(e);
         }
 
@@ -39,11 +42,11 @@ public class Handler implements RequestHandler<String, String> {
         try {
             id = documentDao.save(new DocumentEntity(message));
         } catch (Exception e) {
-            LOG.log("Exception in handleRequest method occurred during Database interaction: " + e);
+            LOG.error("Exception in handleRequest method occurred during Database interaction: " + e);
             throw new RuntimeException(e);
         }
 
-        LOG.log("Request handler with id " + id + " is finishing with message: " + message);
+        LOG.info("Request handler with id " + id + " is finishing with message: " + message);
         return id.toString();
     }
 }
--- src/main/java/org/example/untitled/mq/Mq.java
+++ src/main/java/org/example/untitled/mq/Mq.java
@@ -1,41 +1,32 @@
 package org.example.untitled.mq;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
 
 import javax.jms.*;
 
 
 public class Mq {
 
-    private static final String WIRE_LEVEL_ENDPOINT
-            = "ssl://b-3eb87df2-ac2e-4db8-b0ba-6ce90f4f2a9e-1.mq.eu-central-1.amazonaws.com:61617";
-    private static final String ACTIVE_MQ_USERNAME = "activemq";
-    private static final String ACTIVE_MQ_PASSWORD = "exampleexample";
-    private static final String QUEUE_NAME = "queue";
-
-    private static final PooledConnectionFactory pooledConnectionFactory;
+    private static final String BROKER_URL
+            = "ssl://b-110bb360-7059-454a-8d13-08ba63a3a53f-1.mq.eu-central-1.amazonaws.com:61617";
+    private static final String BROKER_USERNAME = "activemq";
+    private static final String BROKER_PASSWORD = "exampleexample";
+    private static final String BROKER_QUEUE = "queue";
 
     private static final ActiveMQConnectionFactory connectionFactory;
 
     static {
-        connectionFactory = new ActiveMQConnectionFactory(WIRE_LEVEL_ENDPOINT);
-        connectionFactory.setUserName(ACTIVE_MQ_USERNAME);
-        connectionFactory.setPassword(ACTIVE_MQ_PASSWORD);
-
-        pooledConnectionFactory = new PooledConnectionFactory();
-        pooledConnectionFactory.setConnectionFactory(connectionFactory);
-        pooledConnectionFactory.setMaxConnections(10);
+        connectionFactory = new ActiveMQConnectionFactory(BROKER_USERNAME, BROKER_PASSWORD, BROKER_URL);
     }
 
     public static void sendMessage(String message) throws JMSException {
         Connection producerConnection = null;
         try {
-            producerConnection = pooledConnectionFactory.createConnection();
+            producerConnection = connectionFactory.createConnection();
             producerConnection.start();
 
             Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            final Destination producerDestination = producerSession.createQueue(QUEUE_NAME);
+            final Destination producerDestination = producerSession.createQueue(BROKER_QUEUE);
 
             MessageProducer producer = producerSession.createProducer(producerDestination);
             producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
@@ -55,7 +46,7 @@ public class Mq {
             consumerConnection.start();
 
             Session consumerSession = consumerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            final Destination consumerDestination = consumerSession.createQueue(QUEUE_NAME);
+            final Destination consumerDestination = consumerSession.createQueue(BROKER_QUEUE);
 
             MessageConsumer consumer = consumerSession.createConsumer(consumerDestination);
             final TextMessage consumerMessage = (TextMessage) consumer.receive(1000);
